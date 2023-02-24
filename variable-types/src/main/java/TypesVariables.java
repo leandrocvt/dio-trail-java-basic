a022160dcb6d4164f89e93db3a453d77a9b9c133
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2023-02-24 16:54:35 -0300

    Types Variables

---
+++ variable-types/src/main/java/TypesVariables.java
@@ -0,0 +1,18 @@
+public class TypesVariables {
+    public static void main(String[] args) {
+
+        String myName = "Leandro Cavalcanti";
+
+        double basicSalary = 2500;
+
+        short curtNumber = 1;
+        int normalNumber = curtNumber;
+
+        // (short) Downcasting
+        short curtNumber2 = (short) normalNumber;
+
+        //Constantes -> são valores armazenados em memória que não pode ser modificados depois de declarados.
+        final double VALUE_OF_PI = 3.14;
+
+    }
+}
