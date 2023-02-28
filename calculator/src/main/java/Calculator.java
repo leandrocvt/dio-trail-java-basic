5cdd17e24a1f23a5121ab2edb193571f93c8b155
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2023-02-28 11:35:26 -0300

    Java documentation

---
+++ calculator/src/main/java/Calculator.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ *
+ * @author  Leandro Cavalcanti
+ * @version 1.0
+ * @since   28/02/2023
+ */
+public class Calculator {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numberOne este é o primeiro parâmetro do método
+     * @param numberTwo este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int sum(int numberOne, int numberTwo) {
+        return  numberOne + numberTwo;
+    }
+}
---
+++ calculator/src/main/java/Comments.java
@@ -0,0 +1,20 @@
+public class Comments {
+    public static void main(String[] args) {
+        // Comentário de única linha
+
+        /* Olá,
+        * Comentário de várias linhas
+        * Tudo bem?
+        */
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+    public void method(){
+
+    }
+}
